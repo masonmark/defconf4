653aa654663d156170415f5216ff3bb6945cc43c
Mason Mark <mason@fivespeed.com>
2011-04-27 11:56:22 +0900

    Bundle w/ Bundler.
     - My intention was to bundle things such that there are no external dependencies except Ruby itself (fuckoff unless 1.9.2+). I used the slop gem as my little test and I think it worked.

--- src/defcon4.rb
+++ src/defcon4.rb
@@ -1,4 +1,30 @@
 #! /usr/bin/env ruby
 # encoding: utf-8
 
-require 'me to write this in objc becoz that would be like 5 jillion times faster than making me write it in r00by'
+require 'slop'
+
+puts '☢ DEFCONF4 INITIATED ☢'
+
+
+opts = Slop.parse do
+  option :v, :verbose
+  option :h, :help
+end
+
+# opts = opts.to_hash
+
+if opts.help? # or, opts[:help]
+  puts opts.help
+else
+  if opts.verbose?
+    puts '- Verbose mode is enabled.'
+  end
+  puts '-- Determining correct course of action...'
+  sleep 1
+  puts '-- Confused. Giving up.'
+  sleep 1
+  puts '-- Launching nukes.'
+  sleep 1
+  puts '-- BRZzzt$$#%^@%^&**)))))) )  )   )     )        )'
+end
+
