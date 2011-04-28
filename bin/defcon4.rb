eee8e85a6601c963938d083776c26d253581ab10
Mason Mark <mason@fivespeed.com>
2011-04-28 14:31:51 +0900

    Institute new launch procedures.

--- bin/defcon4.rb
+++
@@ -1,1 +0,0 @@
-../src/defcon4.rb
---
+++ bin/defconf4.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby -w
+#encoding: utf-8
+
+require 'pathname'
+
+path_to_me       = Pathname.new(File.expand_path(__FILE__))
+path_to_defconf4 = path_to_me.parent.parent
+path_to_src      = path_to_defconf4 + "src"
+
+Dir.chdir path_to_src 
+
+Kernel.exec "PATH=#{ENV['PATH']}:$PATH ; bundle exec ./initiate_defconf4.rb"
+  # If we don't capture the PATH and pass it to the exec environment, Bundler will puke blood if it happens to be invoked on a system that uses rvm or otherwise has ruby at some whacked out location.
