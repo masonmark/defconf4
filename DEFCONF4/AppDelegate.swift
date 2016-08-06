b2928eaf835f537591bf9c0c6ff238d01aca6a03
Mason Mark <mason@fivespeed.com>
2016-08-06 16:30:11 +0900

    Nuke ruby from orbit.

---
+++ DEFCONF4/AppDelegate.swift
@@ -0,0 +1,21 @@
+// DEFCONF4 Created by mason on 2016-08-06. Copyright © 2016 MASON MARK (.COM). All rights reserved.
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
