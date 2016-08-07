a146ee7b9aff4f996e5b33709d42cd40d4b0f6f8
Mason Mark <mason@fivespeed.com>
2016-08-07 09:58:31 +0900

    Regenerate skeleton.
    And add missing internal organs.

--- DEFCONF4/AppDelegate.swift
+++ DEFCONF4/AppDelegate.swift
@@ -1,11 +1,10 @@
-// DEFCONF4 Created by mason on 2016-08-06. Copyright © 2016 MASON MARK (.COM). All rights reserved.
+//AppDelegate.swift Created by mason on 2016-08-07. Copyright © 2016 MASON MARK (.COM). All rights reserved.
 
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
